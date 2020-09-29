0d881a7bd3f47db3684b099eea7b77d24c20906d
410406130@qq.com <410406130@qq.com>
2020-09-29 20:37:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const http = require('http')
+const webhookHandler = require('github-webhook-handler')
 
+const handler = webhookHandler({path: '/webhook', secret: 'xujunlin'})
 http.createServer((req, res)=>{
 
+    handler(req, res, (err)=>{
 
+        res.statusCode = 404
+        res.end('no')
+    })
 
-    res.end('haode')
 }).listen(8888)
+handler.on('error', (err)=>{
+    console.error('Error:', err.message);
+})
+handler.on('push', (event)=>{
+    console.log('push for %s to %s', event.payload);
+})
+handler.on('issues', (event)=>{
+
+    console.log('issue for %s action=%s ', event.payload);
+
+})
